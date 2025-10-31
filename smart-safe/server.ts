775e79cc0c98fd75c29ebd925124365dcfce5f6c
humblefool13 <akshatksyp13@gmail.com>
2025-10-31 13:10:35 +0530

    port change

--- smart-safe/server.ts
+++ smart-safe/server.ts
@@ -6,7 +6,7 @@ import { Server as SocketIOServer } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = parseInt(process.env.PORT || "3000", 10);
+const port = parseInt(process.env.PORT || "8787", 10);
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
